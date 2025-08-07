686911cdc8065691a455fd5de62de819ddc1f714
Higor Palmeira <higorpresente05@gmail.com>
2025-08-06 22:21:04 -0300

    Atualizando busca.c

--- algoritmos/busca.c
+++ algoritmos/busca.c
@@ -17,6 +17,35 @@ void busca_linear(int array[], int n) {
 
 }
 
+void busca_binaria(int array[], int n) {
+
+    int valor = gerar_valor_aleatorio();
+
+    int baixo = 0;
+    int alto = n - 1;
+
+    while(baixo <= alto) {
+
+        int meio = baixo + (alto - baixo) / 2;
+
+        if (array[meio] == valor) {
+            return;
+        }
+
+        if (array[meio] < valor) {
+            
+            baixo = meio + 1;
+        
+        } else {
+        
+            alto = meio - 1;
+        
+        }
+
+    }
+
+}
+
 int gerar_valor_aleatorio() {
     
     return rand();
